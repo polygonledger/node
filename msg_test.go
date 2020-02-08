ddbf98a9ff24147387a481d304e643df09622603
benjyz <ben@enet.io>
2020-02-08 12:22:56 +0100

    msg types

--- msg_test.go
+++ msg_test.go
@@ -4,22 +4,60 @@ import (
 	"encoding/json"
 	"testing"
 
+	block "github.com/polygonledger/node/block"
 	protocol "github.com/polygonledger/node/net"
 )
 
-func TestAverage(t *testing.T) {
+func TestMessageJson(t *testing.T) {
 
 	msg := protocol.Message{MessageType: "msg", Command: "CMD"}
 	msgJson, _ := json.Marshal(msg)
-	//fmt.Println(string(msgJson))
 
 	var msgUn protocol.Message
 	if err := json.Unmarshal(msgJson, &msgUn); err != nil {
 		panic(err)
 	}
-	//fmt.Println(msgUn.Command, msgUn.MessageType)
 
 	if msgUn.Command != "CMD" {
 		t.Error("JSON marshal failed")
 	}
+
+	if msgUn.MessageType != "msg" {
+		t.Error("JSON marshal failed")
+	}
+}
+
+func TestMessageType(t *testing.T) {
+	msg := protocol.RequestMessage()
+	if msg.MessageType != protocol.REQ {
+		t.Error("msg failed")
+	}
+
+	msg = protocol.ReplyMessage()
+	if msg.MessageType != protocol.REP {
+		t.Error("msg failed")
+	}
+
+	//msgJson, _ := json.Marshal(msg)
+
+}
+
+func TestMessageAccount(t *testing.T) {
+	a := block.Account{AccountKey: "test"}
+	msg := protocol.AccountMessage(a)
+	if msg.Account != a {
+		t.Error("msg failed")
+	}
+
+	msgJson, _ := json.Marshal(msg)
+
+	var msgUn protocol.MessageAccount
+	if err := json.Unmarshal(msgJson, &msgUn); err != nil {
+		panic(err)
+	}
+
+	if msgUn.Account.AccountKey != a.AccountKey {
+		t.Error("JSON marshal failed")
+	}
+
 }
--- net/message.go
+++ net/message.go
@@ -1,9 +1,43 @@
 package net
 
+import block "github.com/polygonledger/node/block"
+
+//Message Types
+//Request <--> Reply
+const (
+	REQ = "REQ"
+	REP = "REP"
+)
+
 //generic message
 type Message struct {
-	MessageType string //Request <--> Reply
+	MessageType string
 	Command     string //Specific message command
 	//Data        []byte
 	//Signature       btcec.Signature
+}
+
+func RequestMessage() Message {
+	return Message{MessageType: REQ}
+}
+
+func ReplyMessage() Message {
+	return Message{MessageType: REP}
+}
+
+type MessageTx struct {
+	MessageType string
+	Command     string
+	Tx          block.Tx
+}
+
+type MessageAccount struct {
+	MessageType string
+	Command     string
+	Account     block.Account
+}
+
+func AccountMessage(account block.Account) MessageAccount {
+	msg := MessageAccount{MessageType: REP, Command: "REP_ACCOUNT", Account: account}
+	return msg
 }
