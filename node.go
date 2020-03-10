c4bcb6bc4f58812fd7578a0651f662464eb4695c
benjyz <ben@enet.io>
2020-03-11 00:54:10 +0700

    reorg web

--- node.go
+++ node.go
@@ -309,28 +309,8 @@ func (t *TCPNode) handleConnection(mgr *chain.ChainManager, ntchan ntcl.Ntchan) 
 
 }
 
-//HTTP
-func LoadContent(mgr *chain.ChainManager) string {
+func BlockContent(mgr *chain.ChainManager) string {
 	content := ""
-
-	// content += fmt.Sprintf("<h2>Peers</h2>Peers: %d<br>", len(peers))
-	// for i := 0; i < len(peers); i++ {
-	// 	content += fmt.Sprintf("peer ip address: %s<br>", peers[i].Address)
-	// }
-
-	content += fmt.Sprintf("<h2>TxPool</h2>%d<br>", len(mgr.Tx_pool))
-
-	for i := 0; i < len(mgr.Tx_pool); i++ {
-		//content += fmt.Sprintf("Nonce %d, Id %x<br>", chain.Tx_pool[i].Nonce, chain.Tx_pool[i].Id[:])
-		ctx := mgr.Tx_pool[i]
-		content += fmt.Sprintf("%d from %s to %s %x<br>", ctx.Amount, ctx.Sender, ctx.Receiver, ctx.Id)
-	}
-
-	content += fmt.Sprintf("<h2>Accounts</h2>number of accounts: %d<br><br>", len(mgr.Accounts))
-
-	for k, v := range mgr.Accounts {
-		content += fmt.Sprintf("%s %d<br>", k, v)
-	}
 
 	content += fmt.Sprintf("<br><h2>Blocks</h2><i>number of blocks %d</i><br>", len(mgr.Blocks))
 
@@ -351,8 +331,54 @@ func LoadContent(mgr *chain.ChainManager) string {
 			content += fmt.Sprintf("%d from %s to %s %x<br>", ctx.Amount, ctx.Sender, ctx.Receiver, ctx.Id)
 		}
 	}
+	return content
+}
+
+func AccountContent(mgr *chain.ChainManager) string {
+
+	content := ""
+	content += fmt.Sprintf("<h2>Accounts</h2>number of accounts: %d<br><br>", len(mgr.Accounts))
+
+	for k, v := range mgr.Accounts {
+		content += fmt.Sprintf("%s %d<br>", k, v)
+	}
+	return content
+}
+
+//HTTP
+func LoadContent(mgr *chain.ChainManager) string {
+	content := ""
+
+	// content += fmt.Sprintf("<h2>Peers</h2>Peers: %d<br>", len(peers))
+	// for i := 0; i < len(peers); i++ {
+	// 	content += fmt.Sprintf("peer ip address: %s<br>", peers[i].Address)
+	// }
+
+	content += fmt.Sprintf("<h2>TxPool</h2>%d<br>", len(mgr.Tx_pool))
+
+	for i := 0; i < len(mgr.Tx_pool); i++ {
+		//content += fmt.Sprintf("Nonce %d, Id %x<br>", chain.Tx_pool[i].Nonce, chain.Tx_pool[i].Id[:])
+		ctx := mgr.Tx_pool[i]
+		content += fmt.Sprintf("%d from %s to %s %x<br>", ctx.Amount, ctx.Sender, ctx.Receiver, ctx.Id)
+	}
+
+	content += "<br>"
+
+	content += "<a href=\"/blocks\">blocks</a><br>"
+	content += "<a href=\"/accounts\">accounts</a><br>"
+
+	//content += BlockContent(mgr)
 
 	return content
+}
+
+func StatusContent(mgr *chain.ChainManager, t *TCPNode) []byte {
+
+	uptimedur := time.Now().Sub(t.Starttime)
+	uptime := int64(uptimedur / time.Second)
+	status := Status{Blockheight: len(mgr.Blocks), Starttime: t.Starttime, Uptime: uptime}
+	jData, _ := json.Marshal(status)
+	return jData
 }
 
 type Status struct {
@@ -368,19 +394,29 @@ func Runweb(t *TCPNode, mgr *chain.ChainManager, webport int) {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := LoadContent(mgr)
+		statusdata := StatusContent(mgr, t)
 		//nlog.Print(p)
-		fmt.Fprintf(w, "<h1>Polygon chain</h1><div>%s</div>", p)
+		fmt.Fprintf(w, "<h1>Polygon chain</h1>Status%s<br><div>%s </div>", statusdata, p)
+	})
+
+	http.HandleFunc("/blocks", func(w http.ResponseWriter, r *http.Request) {
+		p := BlockContent(mgr)
+		//nlog.Print(p)
+		fmt.Fprintf(w, "<div>%s</div>", p)
+	})
+
+	http.HandleFunc("/accounts", func(w http.ResponseWriter, r *http.Request) {
+		p := AccountContent(mgr)
+		//nlog.Print(p)
+		fmt.Fprintf(w, "<div>%s</div>", p)
 	})
 
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		uptimedur := time.Now().Sub(t.Starttime)
-		uptime := int64(uptimedur / time.Second)
-		status := Status{Blockheight: len(mgr.Blocks), Starttime: t.Starttime, Uptime: uptime}
-		jData, _ := json.Marshal(status)
 
-		log.Println(jData)
+		statusdata := StatusContent(mgr, t)
+		log.Println(statusdata)
 		w.Header().Set("Content-Type", "application/json")
-		w.Write(jData)
+		w.Write(statusdata)
 
 		//w.WriteHeader(http.StatusCreated)
 		//json.NewEncoder(w).Encode(status)
