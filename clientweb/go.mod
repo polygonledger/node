aa122f9c7e87b13da8a4cf1eb43a5990ec10b98a
benjyz <ben@enet.io>
2020-05-02 17:09:31 +0700

    basic web client

--- clientweb/go.mod
+++ clientweb/go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/gin-gonic/gin v1.6.2
 	github.com/jinzhu/gorm v1.9.12 // indirect
+	github.com/polygonledger/node v0.0.0-20200502072124-46b566873313
 )
--- clientweb/main.go
+++ clientweb/main.go
@@ -1,29 +1,71 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/polygonledger/node/crypto"
 )
 
+var pw string
+
+type PageData struct {
+	PageTitle string
+	Password  string
+	Pubkey    string
+	Privkey   string
+	Address   string
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("basic.html"))
+
+	kp := crypto.PairFromSecret(pw)
+	pubkeyHex := crypto.PubKeyToHex(kp.PubKey)
+	privHex := crypto.PrivKeyToHex(kp.PrivKey)
+	address := crypto.Address(pubkeyHex)
+
+	data := PageData{
+		PageTitle: "Polygon client",
+		Password:  pw,
+		Pubkey:    pubkeyHex,
+		Privkey:   privHex,
+		Address:   address,
+	}
+
+	//tmpl.Execute(w, data)
+
+	switch r.Method {
+	case "GET":
+		//http.ServeFile(w, r, "form.html")
+		tmpl.Execute(w, data)
+	case "POST":
+		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+		pw = r.FormValue("pw")
+		fmt.Fprintf(w, "pw = %s\n", pw)
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
 func main() {
-	r := gin.Default()
+	pw = "default"
+	http.HandleFunc("/", hello)
 
-	r.LoadHTMLGlob("templates/*")
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
-
-	//curl --data "name=bla" http://localhost:8080/name
-	r.GET("/name", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", []string{"a", "b", "c"})
-	})
-	r.POST("/name", func(c *gin.Context) {
-		//name := c.Params.ByName("name")
-		//c.JSON(http.StatusOK, gin.H{"name": name})
-		c.JSON(http.StatusOK, gin.H{"name": "name"})
-	})
-
-	r.Run()
+	fmt.Printf("Starting server for testing HTTP POST...\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
