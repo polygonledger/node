89dafce069ba850e09aa755f1c3b0674f396d86a
benjyz <ben@enet.io>
2020-05-25 11:52:28 +0700

    comment app

--- handlers_app.go
+++ handlers_app.go
@@ -1,5 +1,17 @@
 package main
 
+//this namespace is application specific will ultimately live separated from core e.g. polygon-services
+
+//trivial simplified chat service
+//clients need to subscribe with {"MESSAGETYPE":"REQ","COMMAND":"SUBTO"}
+//clients send a REQ message
+
+//more abstract note
+//in ETH contracts are handled without knowledge and executed blindly
+//we want everyone to be able to deploy contracts permissionsless. however, this completely ignored the problem of competing claims
+//if say e.g. google, facebook, amazon or any public entity would deploy a contract, reserve a name etc. how we deal with conflicts?
+//we want to be able to handle different trust paradigms and deal with real world property
+
 import (
 	"encoding/json"
 	"fmt"
@@ -7,14 +19,14 @@ import (
 	"github.com/polygonledger/node/netio"
 )
 
-//TODO in handlers_app.go
 func HandleChat(t *TCPNode, ntchan netio.Ntchan, req_msg netio.Message) string {
 
 	xjson, _ := json.Marshal("hello chat world")
 	msg := netio.Message{MessageType: netio.REP, Command: netio.CMD_CHAT, Data: []byte(xjson)}
 	reply_msg := netio.ToJSONMessage(msg)
 
-	//TODO this should be in publoop
+	//TODO this should be in pub chan not REQ out
+	//don't publish to author
 	for _, subs := range t.ChatSubscribers {
 		pub_msg_string := fmt.Sprintf("%v said: publish chat world", ntchan)
 
